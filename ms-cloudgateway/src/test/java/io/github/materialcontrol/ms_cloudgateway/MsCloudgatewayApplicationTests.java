2453454aa01493c481a9c56b85c4f72bfc1ab66f
jowgaze <jonatha.biomon.hp@gmail.com>
2025-01-10 17:12:24 -0300

    feat: create and configure cloud gateway for access ms-users and ms-materials

---
+++ ms-cloudgateway/src/test/java/io/github/materialcontrol/ms_cloudgateway/MsCloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.materialcontrol.ms_cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
